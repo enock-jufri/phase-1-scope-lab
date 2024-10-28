5dece9116960fb14bc995da2a9ca7bfa911725ad
enock-jufri <enockjufri@gmail.com>
2024-10-28 12:08:59 +0300

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'luke'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(x){
+    bestCustomer = "maybe bob"
+
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'neo'
+}
